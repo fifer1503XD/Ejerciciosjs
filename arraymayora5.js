48dcfe3c037a866fd3c88d25e54c6c0d47094952
fiferortiz <pedrofelipeortiz@gmail.com>
2020-09-23 21:04:56 -0500

    problema arreglo

--- arraymayora5.js
+++ arraymayora5.js
@@ -1,10 +1,12 @@
 
+function give(){
 for( var i=0; i<6; i=i){
     var num = prompt("indica el numero de posiciones del arreglo");
     i=num;
     if(i<6){
         alert("Escriba un numero mayor de 5");
     }
+ }
 }
 array=[];
 for( var i=0; i<num ;i++){
@@ -28,5 +30,6 @@ function sum1(data) {
     alert('la suma de los números del arreglo sin sus primeras 5 posiciones es :'+d);
     return d; 
    }
+give()
 sum1(array)
 sum2(array)
